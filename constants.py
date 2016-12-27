9b84063d85364aaedd820e0ce483516a9f299143
Francisco Cardoso <franciscotcardoso@hotmail.com>
2016-12-27 16:34:04 +0000

    Added code initial commit

---
+++ constants.py
@@ -0,0 +1,10 @@
+HOST = 'msg2.embers.citibrain.com'
+
+DEVICE = {
+    'uuid': '0cb35b3c-c57b-45e7-8068-69bf194126e9',
+    'token': '94a6272eb89344ff1bc841e5fc6c2968d14fcc51',
+}
+
+GATEWAY = {
+    'uuid': '9cac9902-cff4-41a7-9c3e-c50b6d9d7f32'
+}
